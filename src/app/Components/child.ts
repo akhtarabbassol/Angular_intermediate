23bf5a246ac1adc8d3f3530b2e6cdd0b759f6703
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2026-08-26 17:01:23 +0500

    Two Way Binding

--- src/app/Components/child.ts
+++ src/app/Components/child.ts
@@ -1,13 +1,13 @@
-import { Component, input } from "@angular/core";
-import { userinfo } from "../app";
-
+import { Component, input, output } from "@angular/core";
 
 @Component({
-    selector:'child-app' ,
-    templateUrl:'child.html',
-    styleUrl:'child.css'
+  selector:"child-app",
+  templateUrl:'child.html',
+  styleUrl:'child.css'
 })
 
-export class childApp{
-  user1=input<userinfo>()
+export class ChildApp{
+  Product:string = "Mobile"
+  Quentity=input<number>(0) 
+  price=input<number>(0)
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,34 +1,14 @@
 import { Component } from "@angular/core";
-import { childApp } from "./Components/child";
+import { ChildApp } from "./Components/child";
 
-
-export interface userinfo{
-  Name  : string ;
-  Email : string ;
-  Phone : number ;
-}
 
 @Component({
   selector:'app-root',
-  imports:[childApp] ,
   templateUrl:'app.html',
-  styleUrl:'app.css'  
+  styleUrl:'app.css',
+  imports: [ChildApp]
 })
 
 export class App{
-  username:string = ""
-  useremail:string = ""
-  userphone:number = 0
-  User:userinfo = {
-    Name:'' ,
-    Email: '',
-    Phone: 0
-  }
-  SendData(username:string , useremail:string , userphone:number){
-   this.User={
-    Name : username ,
-    Email: useremail ,
-    Phone : userphone ,
-   }
-  }
+
 }
